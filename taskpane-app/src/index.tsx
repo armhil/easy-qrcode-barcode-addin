79b8ccac9442147f888ff4bebb7d6e4aeb852e43
armhil <arman.hilmioglu@gmail.com>
2024-03-12 21:01:09 +0000

    fix file name

--- taskpane-app/src/index.tsx
+++ taskpane-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './app1';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
 ReactDOM.render(
